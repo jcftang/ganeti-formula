291bf78c4a13e510242ac8613a636eebc9127d33
Jimmy Tang <jcftang@gmail.com>
2015-02-04 09:12:02 +0000

    Notes

--- ganeti/files/setup-ipt.sh
+++ ganeti/files/setup-ipt.sh
@@ -1,3 +1,18 @@
+# See https://people.debian.org/~ultrotter/talks/dc9/dc9_ganeti_talk.html - it's a nice way of setting up a dev system in terms of networking.
+# cat >>/etc/network/interfaces <<EOF
+# iface dummy0 inet static
+# address 10.0.4.254
+# netmask 255.255.255.0
+#
+# iface br0 inet static
+# address 10.0.4.254
+# netmask 255.255.255.0
+# bridge_ports dummy0
+# pre-up /sbin/ifup dummy0
+# up /etc/init.d/dnsmasq restart
+# down /etc/init.d/dnsmasq stop
+# EOF
+
 $IPT -N FWD_VIRTUAL
 $IPT -t nat -N PST_VIRTUAL
 
